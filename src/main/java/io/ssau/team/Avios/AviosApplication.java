89e69238d88e276a37140e8be5264e1e3a5f2cc0
Tagir <tagirman998@gmail.com>
2019-03-06 23:21:00 +0400

    init

---
+++ src/main/java/io/ssau/team/Avios/AviosApplication.java
@@ -0,0 +1,13 @@
+package io.ssau.team.Avios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviosApplication.class, args);
+	}
+
+}
